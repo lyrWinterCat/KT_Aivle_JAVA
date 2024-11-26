45a2142fb9f0683d43fcc3281882d3cf8e739a6c
WinterCat <dldulfla27@gmail.com>
2024-11-26 17:56:58 +0900

    1st-day

---
+++ chap01/src/com/example/chap01/IntegerValues.java
@@ -0,0 +1,16 @@
+package com.example.chap01;
+
+public class IntegerValues {
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = 101;
+        int c = 102;
+        long d = 103;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        
+    }
+}
