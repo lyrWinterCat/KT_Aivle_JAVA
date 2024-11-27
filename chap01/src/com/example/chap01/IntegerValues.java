58939175b4e0ccc8607332027dd31055b343a757
WinterCat <dldulfla27@gmail.com>
2024-11-27 09:48:23 +0900

    chap01

--- chap01/src/com/example/chap01/IntegerValues.java
+++ chap01/src/com/example/chap01/IntegerValues.java
@@ -2,15 +2,15 @@ package com.example.chap01;
 
 public class IntegerValues {
     public static void main(String[] args) {
-        byte a = 100;
-        short b = 101;
-        int c = 102;
-        long d = 103;
+        byte a = Byte.MAX_VALUE;
+        short b = Short.MAX_VALUE;
+        int c = Integer.MAX_VALUE;
+        long d = Long.MAX_VALUE;
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println("c = " + c);
-        System.out.println("d = " + d);
-        
+        System.out.println("byte a의 값은 " + a);
+        System.out.println("short b의 값은 " + b);
+        System.out.println("int c의 값은 " + c);
+        System.out.println("long d의 값은 " + d);
+
     }
 }
